ec4af752239ab1fe05896382813dd203e6747dd5
zbartkus <zbartkuss3150@gmail.com>
2020-05-18 22:04:37 +0200

    hotfix

--- state.py
+++ state.py
@@ -11,51 +11,51 @@ class State:
         if self.end is not None:
             return self.end       
         for i in range(0, len(self.data), 2):
-            x,y = data[i]
+            x,y = self.data[i]
             for j in range(i+2, len(self.data), 2):
-                x2,y2 = data[j]
+                x2,y2 = self.data[j]
                 if (x == x2 + 1 and (y == y2 or y == y2+1 or y == y2-1)) or (x == x2 - 1 and (y == y2 or y == y2+1 or y == y2-1)) or (x == x2 and (y == y2-1 or y == y2+1)):
                     xdiff = x-x2
                     ydiff = y-y2
                     for k in range(j+2, len(self.data), 2):
-                        x3,y3 = data[k]
+                        x3,y3 = self.data[k]
                         if np.abs(xdiff) == 1 and ydiff == 0:
                             if y == y3 and (x == x3 - xdiff or x2 == x3 + xdiff):
-                                self.winner = True
+                                self.winner = 1
                                 self.end = True
                                 return self.end
                         elif xdiff == 0 and np.abs(ydiff) == 1:
-                            if x == x3 and (y == y3 - ydiff or y2 == y3 + diff):
-                                self.winner = True
+                            if x == x3 and (y == y3 - ydiff or y2 == y3 + ydiff):
+                                self.winner = 1
                                 self.end = True
                                 return self.end
                         else:
-                            if (x == x3 - xdiff and y == y3 - diff) or (x2 == x3 + xdiff and y2 == y3 + ydiff):
-                                self.winner = True
+                            if (x == x3 - xdiff and y == y3 - ydiff) or (x2 == x3 + xdiff and y2 == y3 + ydiff):
+                                self.winner = 1
                                 self.end = True
                                 return self.end
         for i in range(1, len(self.data), 2):
-            x,y = data[i]
+            x,y = self.data[i]
             for j in range(i+2, len(self.data), 2):
-                x2,y2 = data[j]
+                x2,y2 = self.data[j]
                 if (x == x2 + 1 and (y == y2 or y == y2+1 or y == y2-1)) or (x == x2 - 1 and (y == y2 or y == y2+1 or y == y2-1)) or (x == x2 and (y == y2-1 or y == y2+1)):
                     xdiff = x-x2
                     ydiff = y-y2
                     for k in range(j+2, len(self.data), 2):
-                        x3,y3 = data[k]
+                        x3,y3 = self.data[k]
                         if np.abs(xdiff) == 1 and ydiff == 0:
                             if y == y3 and (x == x3 - xdiff or x2 == x3 + xdiff):
-                                self.winner = False
+                                self.winner = 2
                                 self.end = True
                                 return self.end
                         elif xdiff == 0 and np.abs(ydiff) == 1:
-                            if x == x3 and (y == y3 - ydiff or y2 == y3 + diff):
-                                self.winner = False
+                            if x == x3 and (y == y3 - ydiff or y2 == y3 + ydiff):
+                                self.winner = 2
                                 self.end = True
                                 return self.end
                         else:
-                            if (x == x3 - xdiff and y == y3 - diff) or (x2 == x3 + xdiff and y2 == y3 + ydiff):
-                                self.winner = False
+                            if (x == x3 - xdiff and y == y3 - ydiff) or (x2 == x3 + xdiff and y2 == y3 + ydiff):
+                                self.winner = 2
                                 self.end = True
                                 return self.end
         self.end = False
