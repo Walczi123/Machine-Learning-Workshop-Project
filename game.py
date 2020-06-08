548fe50a799210b59a00091d3f46a9c8a5734a4c
Patryk <patryk.wal@onet.pl>
2020-06-08 20:37:47 +0200

    fixing

--- game.py
+++ game.py
@@ -2,14 +2,16 @@ from player import Player, Bot
 from state import State
 
 class Game:
-    def __init__(self, p1 = Player(), p2 =Player(), need_for_win = 3):
+    def __init__(self, p1 = Player(), p2 =Player(), need_for_win = 3, debug = False):
         self.player1 = p1
         self.player2 = p2
         self.state = State(need_for_win)
+        self.need_for_win = need_for_win
         self.currentPlayer = None
+        self.debug = debug
 
     def reset(self):
-        self.state = State()
+        self.state = State(self.need_for_win)
 
     def play(self):
         self.currentPlayer = self.player1
@@ -24,8 +26,7 @@ class Game:
             else :
                 self.currentPlayer = self.player1  
                 label = "X"  
-            #self.state.isEnd()
-            self.state.martinIsEnd()
+            self.state.isEnd()
             self.state.printVector()
         print("--- END ---")
         print("Player "+str(self.state.winner)+" wins")
@@ -43,25 +44,24 @@ class Game:
             else :
                 self.currentPlayer = self.player1  
                 label = "X"  
-            #self.state.isEnd()
-            self.state.martinIsEnd()
+            self.state.isEnd()
             self.state.printVector()
-        print("--- END ---")
-        print("Player "+str(self.state.winner)+" wins")
+        if(self.debug):
+            print("Player "+str(self.state.winner)+" wins")
 
-    def train(self, epochs=20000):
+    def train(self, iterations=20000):
         player1Win = 0.0
         player2Win = 0.0
-        for i in range(0, epochs):
-            print("Epoch", i)
+        for i in range(0, iterations):
+            print("Epoch", i/iterations)
             self.play()
             if self.state.winner == 1:
                 player1Win += 1
             if self.state.winner  == 2:
                 player2Win += 1
             self.reset()
-        print(player1Win / epochs)
-        print(player2Win / epochs)
+        print(player1Win / iterations)
+        print(player2Win / iterations)
         # player1.savePolicy()
         # player2.savePolicy()
 
