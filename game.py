f2d75cbdf84a595a85a2cf246541c3fa36440915
Patryk <patryk.wal@onet.pl>
2020-06-08 19:58:11 +0200

    training

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
-from player import Player
+from player import Player, Bot
 from state import State
 
 class Game:
-    def __init__(self):
-        self.player1 = Player()
-        self.player2 = Player()
+    def __init__(self, p1 = Player(), p2 =Player()):
+        self.player1 = p1
+        self.player2 = p2
         self.state = State()
         self.currentPlayer = None
 
@@ -27,6 +27,41 @@ class Game:
         print("--- END ---")
         print("Player "+str(self.state.winner)+" wins")
 
+    def train_play(self):
+        self.currentPlayer = self.player1
+        while self.state.end != True :
+            move = self.currentPlayer.move()
+            if not self.state.addMove(move):
+                continue;
+            if self.currentPlayer == self.player1 :
+                self.currentPlayer = self.player2
+            else :
+                self.currentPlayer = self.player1    
+            #self.state.isEnd()
+            self.state.martinIsEnd()
+            self.state.printVector()
+        print("--- END ---")
+        print("Player "+str(self.state.winner)+" wins")
+
+    def train(self, epochs=20000):
+        player1Win = 0.0
+        player2Win = 0.0
+        for i in range(0, epochs):
+            print("Epoch", i)
+            self.play()
+            if self.state.winner == 1:
+                player1Win += 1
+            if self.state.winner  == 2:
+                player2Win += 1
+            self.reset()
+        print(player1Win / epochs)
+        print(player2Win / epochs)
+        # player1.savePolicy()
+        # player2.savePolicy()
+
 if __name__ == "__main__":
-    game = Game()
+    b1 = Bot()
+    b2 = Bot()
+    game = Game(b1,b2)
+    game.train()
     game.play()        
--- player.py
+++ player.py
@@ -5,4 +5,36 @@ class Player:
     def move(self):
         x, y = input("Input x and y:  ").split() 
         return (int(x), int(y))
+
+class Bot(Player):
+    def __init__(self):
+        pass
+
+    def move(self):
+        # state = self.states[-1]
+        # nextStates = []
+        # nextPositions = []
+        # for i in range(BOARD_ROWS):
+        #     for j in range(BOARD_COLS):
+        #         if state.data[i, j] == 0:
+        #             nextPositions.append([i, j])
+        #             nextStates.append(state.nextState(i, j, self.symbol).getHash())
+        # if np.random.binomial(1, self.exploreRate):
+        #     np.random.shuffle(nextPositions)
+        #     # Not sure if truncating is the best way to deal with exploratory step
+        #     # Maybe it's better to only skip this step rather than forget all the history
+        #     self.states = []
+        #     action = nextPositions[0]
+        #     action.append(self.symbol)
+        #     return action
+
+        # values = []
+        # for hash, pos in zip(nextStates, nextPositions):
+        #     values.append((self.estimations[hash], pos))
+        # np.random.shuffle(values)
+        # values.sort(key=lambda x: x[0], reverse=True)
+        # action = values[0][1]
+        # action.append(self.symbol)
+        # return action
+        return (1, 1)
     
