6c7851f16d6aca3e5d6cebbdd46c2d2c7a8b3a2e
Patryk <patryk.wal@onet.pl>
2020-06-10 17:58:58 +0200

    2versions

---
+++ Infinite Version/player.py
@@ -0,0 +1,76 @@
+import numpy as np
+from state import findCell
+import pickle
+
+class Player:
+    def __init__(self):
+        pass
+
+    def move(self, state):
+        x, y = input("Input x and y:  ").split() 
+        return (int(x), int(y))
+
+class Bot(Player):
+    def __init__(self, id, limiter = 5,stepSize=0.1, exploreRate = 0.3):
+        self.exploreRate = exploreRate
+        self.states = []
+        self.stepSize = stepSize
+        self.estimations = dict()
+        self.limiter = limiter
+        self.stepSize = stepSize
+        self.id = id
+        pass
+
+    def move(self, state):
+        #state = self.states[-1]
+
+        nextStates = []
+        nextPositions = []
+        for i in range(-self.limiter, self.limiter+1):
+            for j in range(-self.limiter, self.limiter+1):
+                if (i,j) not in state.data:
+                    nextPositions.append((i,j))
+                    nextStates.append(state.nextState(i,j).getHash())
+                    if state.nextState(i,j).getHash() not in self.estimations:
+                        if state.nextState(i,j).isEnd:
+                            if state.nextState(i,j).winner == id:
+                                self.estimations[state.nextState(i,j).getHash()] = 1
+                            else:
+                                self.estimations[state.nextState(i,j).getHash()] = 0
+
+        if np.random.binomial(1, self.exploreRate):
+            np.random.shuffle(nextPositions)
+            self.states=[]
+            action=nextPositions[0]
+            return action
+
+        values = []
+        for hash, pos in zip(nextStates, nextPositions):
+            values.append((self.estimations[hash], pos))
+        np.random.shuffle(values)
+        values.sort(key=lambda x: x[0], reverse=True)
+        action = values[0][1]
+        return action
+    
+    # update estimation according to reward
+    def feedReward(self, reward):
+        if len(self.states) == 0:
+            return
+        self.states = [state.getHash() for state in self.states]
+        target = reward
+        for latestState in reversed(self.states):
+            value = self.estimations[latestState] + self.stepSize * (target - self.estimations[latestState])
+            self.estimations[latestState] = value
+            target = value
+        self.states = []
+
+    def savePolicy(self):
+        fw = open('optimal_policy_' + str(self.id), 'wb')
+        pickle.dump(self.estimations, fw)
+        fw.close()
+
+    def loadPolicy(self):
+        fr = open('optimal_policy_' + str(self.id),'rb')
+        self.estimations = pickle.load(fr)
+        fr.close()
+    
---
+++ Infinite Version/state.py
@@ -0,0 +1,111 @@
+import numpy as np
+import math
+
+class State:
+    def __init__(self, need_for_win):
+        self.data = []
+        self.winner = None
+        self.end = False
+        self.need_for_win = need_for_win
+
+    def addMove(self,move):
+        if findCell(self.data,move[0],move[1]):
+            print("This cell is already occupied! Try again")
+            return False
+        self.data.append(move)
+        return True
+    
+    def isEnd(self):
+        win=self.need_for_win
+        minWin=math.floor((win-1)/2)
+        maxWin=math.ceil(win/2)
+        if self.end is True:
+            return self.end
+        listPlayer1=self.data[0::2]
+        listPlayer2=self.data[1::2]
+        for elem in listPlayer1:
+            if checkCell(elem,listPlayer1,win,minWin,maxWin) == True:
+                self.end=True
+                self.winner=1
+                return self.end
+        for elem in listPlayer2:
+            if checkCell(elem,listPlayer2,win,minWin,maxWin) == True:
+                self.end=True
+                self.winner=2
+                return self.end
+                
+    def printVector(self):
+        vector = self.data
+        list = []
+        for index in range(len(self.data)):
+            if (index%2)==0:
+                label="X"
+            else:
+                label="O"
+            list.append((vector[index][0],vector[index][1],label))
+        maxwidth=max(list,key=lambda x:x[0])[0]
+        minwidth=min(list,key=lambda x:x[0])[0]
+        maxheight=max(list,key=lambda x:x[1])[1]
+        minheight=min(list,key=lambda x:x[1])[1]    
+        print("",end="   ")
+        for y in range(minwidth,maxwidth+1):
+            print(y,end="   ")
+        for y in range(maxheight,minheight-1,-1):
+            print("\n")
+            print(y,end="   ")
+            for x in range(minwidth,maxwidth+1):
+                cell=findCell(list,x,y)
+                if(cell):
+                    print(cell[0][2],end="   ")
+                else:
+                    print(".",end="   ")
+        print("\n")
+        
+    def getHash(self):
+        return hash(frozenset(self.data))
+
+    def nextState(self, i, j):
+        newState = State(need_for_win=3)
+        newState.data = self.data.copy()
+        newState.data.append((i, j))
+        return newState
+
+def findCell(vector,x,y):
+    result = None
+    result=[item for item in vector if item[0]==x and item[1]==y]
+    return result
+
+def checkCell(cell,playerList,win,minWin,maxWin):
+    count=0
+    for shift in range(-minWin,maxWin+1):
+        if [item for item in playerList if item[0]==(cell[0]+shift) and item[1]==(cell[1]+shift)]:
+            count=count+1
+            if count == win:
+                return True
+        else:
+            break
+    count=0
+    for shift in range(maxWin,minWin-1,-1):
+        if [item for item in playerList if item[0]==(cell[0]+shift) and item[1]==(cell[1]+shift)]:
+            count=count+1
+            if count == win:
+                return True
+        else:
+            break
+    count=0
+    for shift in range(-minWin,maxWin+1):
+        if [item for item in playerList if item[0]==(cell[0]+shift) and item[1]==cell[1]]:
+            count=count+1
+            if count == win:
+                return True
+        else:
+            break
+    count=0
+    for shift in range(-minWin,maxWin+1):
+        if [item for item in playerList if item[0]==cell[0] and item[1]==(cell[1]+shift)]:
+            count=count+1
+            if count == win:
+                return True
+        else:
+            break
+    return False
