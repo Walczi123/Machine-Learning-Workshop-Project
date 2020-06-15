acd8adda5be4804286647d90781793a933397070
szymczakf <filip.p.szymczak@gmail.com>
2020-06-15 19:53:19 +0200

    Added alternative infinite version

---
+++ Infinite Version Alt/only_play.py
@@ -0,0 +1,21 @@
+from player import Player, Bot
+from game import Game
+
+#p1 = Player()
+#p2 = Bot(2)
+#p2.loadPolicy()
+p1 = Bot(1)
+p1.loadPolicy()
+p2=Player()
+game = Game(p1, p2, need_for_win=3)
+game.debug = True
+ans = ""
+while(ans != "n"):
+    game.play() 
+    ans = ""
+    while ans != "n" and ans !="y" :
+        if ans != "":
+            print("Please, type correct anwser")
+        ans = input("Would you like to play again? y/n  ")
+    print("Thank you for playing")
+    
